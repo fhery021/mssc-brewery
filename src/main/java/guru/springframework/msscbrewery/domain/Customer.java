3b33050087f55f3de0d8bcfb4578223a74ef051b
Ferenc Bondar <fhery021@gmail.com>
2020-04-14 16:47:35 +0200

    Mapstruct assignment

---
+++ src/main/java/guru/springframework/msscbrewery/domain/Customer.java
@@ -0,0 +1,15 @@
+package guru.springframework.msscbrewery.domain;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import java.util.UUID;
+
+public class Customer {
+
+    private UUID id;
+
+    @NotBlank
+    @Size(min = 3, max = 100)
+    private String name;
+
+}
---
+++ target/generated-sources/annotations/guru/springframework/msscbrewery/mapper/CustomerMapperImpl.java
@@ -0,0 +1,38 @@
+package guru.springframework.msscbrewery.mapper;
+
+import guru.springframework.msscbrewery.domain.Customer;
+import guru.springframework.msscbrewery.web.model.CustomerDto;
+import guru.springframework.msscbrewery.web.model.CustomerDto.CustomerDtoBuilder;
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2020-04-14T16:46:40+0200",
+    comments = "version: 1.3.0.Final, compiler: javac, environment: Java 11.0.6 (JetBrains s.r.o)"
+)
+@Component
+public class CustomerMapperImpl implements CustomerMapper {
+
+    @Override
+    public CustomerDto customerToCustomerDto(Customer customer) {
+        if ( customer == null ) {
+            return null;
+        }
+
+        CustomerDtoBuilder customerDto = CustomerDto.builder();
+
+        return customerDto.build();
+    }
+
+    @Override
+    public Customer customerDtoToCustomer(CustomerDto customerDto) {
+        if ( customerDto == null ) {
+            return null;
+        }
+
+        Customer customer = new Customer();
+
+        return customer;
+    }
+}
